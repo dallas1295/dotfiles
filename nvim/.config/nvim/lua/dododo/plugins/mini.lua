d7ae9cb3f00951665dedcd0a8113214f1704095e
Dallas S <andrewdallassherman@gmail.com>
2025-03-16 10:07:27 +0900

    updates

--- nvim/.config/nvim/lua/dododo/plugins/mini.lua
+++ nvim/.config/nvim/lua/dododo/plugins/mini.lua
@@ -25,7 +25,27 @@ return {
 			},
 		})
 		require("mini.indentscope").setup()
-		-- extra cool stuff
+		require("mini.move").setup({
+			mappings = {
+				-- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+				left = "<M-S-h>",
+				right = "<M-S-l>",
+				down = "<M-S-j>",
+				up = "<M-S-k>",
+
+				-- Move current line in Normal mode
+				line_left = "<M-S-h>",
+				line_right = "<M-S-l>",
+				line_down = "<M-S-j>",
+				line_up = "<M-S-k>",
+			},
+
+			-- Options which control moving behavior
+			options = {
+				-- Automatically reindent selection during linewise vertical move
+				reindent_linewise = true,
+			},
+		})
 		require("mini.git").setup()
 		require("mini.notify").setup()
 	end,
