bb097a2a8694bd772a2fbfa8b829fe14726af149
Dallas S. <andrewdallassherman@gmail.com>
2025-04-10 12:59:11 +0900

    nvim theming, plugin cleanup

--- nvim/.config/nvim/lua/dallas/plugins/mini.lua
+++ nvim/.config/nvim/lua/dallas/plugins/mini.lua
@@ -4,56 +4,35 @@ return {
 	config = function()
 		--appearance
 		require("mini.notify").setup()
-		require("mini.animate").setup()
+		local hi = require("mini.hipatterns")
+		hi.setup({
+			highlighters = {
+				-- Highlighting for comments
+				fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+				hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+				todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+				note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+				-- Hexcode highlighting
+				hex_color = hi.gen_highlighter.hex_color({ priority = 2000 }),
+				shorthand = {
+					pattern = "()#%x%x%x()%f[^%x%w]",
+					group = function(_, _, data)
+						local match = data.full_match
+						local r, g, b = match:sub(2, 2), match:sub(3, 3), match:sub(4, 4)
+						local hex_color = "#" .. r .. r .. g .. g .. b .. b
+
+						return MiniHipatterns.compute_hex_color_group(hex_color, "bg")
+					end,
+					extmark_opts = { priority = 2000 },
+				},
+			},
+		})
 		require("mini.icons").setup()
 		require("mini.statusline").setup()
 		require("mini.tabline").setup()
 		require("mini.indentscope").setup()
-		require("mini.git").setup()
 		require("mini.diff").setup()
-		local miniclue = require("mini.clue")
-		miniclue.setup({
-			triggers = {
-				-- Leader triggers
-				{ mode = "n", keys = "<Leader>" },
-				{ mode = "x", keys = "<Leader>" },
-
-				-- Built-in completion
-				{ mode = "i", keys = "<C-x>" },
-
-				-- `g` key
-				{ mode = "n", keys = "g" },
-				{ mode = "x", keys = "g" },
-
-				-- Marks
-				{ mode = "n", keys = "'" },
-				{ mode = "n", keys = "`" },
-				{ mode = "x", keys = "'" },
-				{ mode = "x", keys = "`" },
-
-				-- Registers
-				{ mode = "n", keys = '"' },
-				{ mode = "x", keys = '"' },
-				{ mode = "i", keys = "<C-r>" },
-				{ mode = "c", keys = "<C-r>" },
-
-				-- Window commands
-				{ mode = "n", keys = "<C-w>" },
-
-				-- `z` key
-				{ mode = "n", keys = "z" },
-				{ mode = "x", keys = "z" },
-			},
-
-			clues = {
-				-- Enhance this by adding descriptions for <Leader> mapping groups
-				miniclue.gen_clues.builtin_completion(),
-				miniclue.gen_clues.g(),
-				miniclue.gen_clues.marks(),
-				miniclue.gen_clues.registers(),
-				miniclue.gen_clues.windows(),
-				miniclue.gen_clues.z(),
-			},
+		require("mini.clue").setup({
 			window = {
 				config = {},
 				delay = 10,
@@ -81,10 +60,7 @@ return {
 		require("mini.snippets").setup()
 		require("mini.surround").setup()
 		require("mini.pairs").setup()
-		require("mini.ai").setup()
 		require("mini.comment").setup()
-		require("mini.trailspace").setup()
-		require("mini.operators").setup()
 		require("mini.cursorword").setup()
 		require("mini.move").setup({
 			mappings = {
