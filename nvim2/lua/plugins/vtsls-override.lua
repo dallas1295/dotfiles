8b1535ce3f049cb95c74e5ce4e42966832239024
Dallas S. <andrewdallassherman@gmail.com>
2025-04-13 13:00:53 +0900

    clean up and yazi

--- nvim2/lua/plugins/vtsls-override.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "neovim/nvim-lspconfig",
-  config = function()
-    local lspconfig = require("lspconfig")
-    lspconfig.vtsls.setup{
-      --empty setup will prevent it from attaching.
-    }
-  end,
-}
