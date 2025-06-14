4e4b1f98deb1200392523dca47cce54be9affee6
Dallas S. <andrewdallassherman@gmail.com>
2025-06-14 09:30:48 +0900

    big lsp inmprovements

--- nvim/.config/nvim/lua/dallas/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/dallas/plugins/colorscheme.lua
@@ -1,16 +1,52 @@
+-- return {
+-- 	"rose-pine/neovim",
+-- 	name = "rose-pine",
+-- 	config = function()
+-- 		require("rose-pine").setup({
+-- 			highlight_groups = {
+-- 				Normal = { bg = "#000000" },
+-- 				NormalNC = { bg = "#000000" },
+-- 				SignColumn = { bg = "#000000" },
+-- 				EndOfBuffer = { bg = "#000000" },
+-- 				MsgArea = { bg = "#000000" },
+-- 			},
+-- 		})
+-- 		vim.cmd("colorscheme rose-pine")
+-- 	end,
+-- }
+--
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	config = function()
-		require("rose-pine").setup({
-			highlight_groups = {
-				Normal = { bg = "#000000" },
-				NormalNC = { bg = "#000000" },
-				SignColumn = { bg = "#000000" },
-				EndOfBuffer = { bg = "#000000" },
-				MsgArea = { bg = "#000000" },
-			},
-		})
-		vim.cmd("colorscheme rose-pine")
-	end,
+  "rebelot/kanagawa.nvim",
+  config = function()
+    -- Default options:
+    require('kanagawa').setup({
+      compile = false,  -- enable compiling the colorscheme
+      undercurl = true, -- enable undercurls
+      commentStyle = { italic = true },
+      functionStyle = {},
+      keywordStyle = { italic = true },
+      statementStyle = { bold = true },
+      typeStyle = {},
+      transparent = false,   -- do not set background color
+      dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
+      terminalColors = true, -- define vim.g.terminal_color_{0,17}
+      colors = {             -- add/modify theme and palette colors
+        palette = {},
+        theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+      },
+      overrides = function(colors) -- add/modify highlights
+        return {
+          Normal       = { bg = "#000000" },
+          NormalNC     = { bg = "#000000" },
+          SignColumn   = { bg = "#000000" },
+          EndOfBuffer  = { bg = "#000000" },
+          LineNr       = { bg = "#000000" },
+          CursorLineNr = { bg = "#000000" },
+          MsgArea      = { bg = "#000000" },
+        }
+      end,
+      theme = "wave", -- Load "wave" theme
+    })
+    vim.cmd("colorscheme kanagawa")
+  end,
 }
