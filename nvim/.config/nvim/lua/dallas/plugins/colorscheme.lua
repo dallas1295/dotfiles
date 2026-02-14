6094c39781facff689a3bbdb91daaabb41f314c1
Dallas S. <andrewdallassherman@gmail.com>
2026-02-14 23:57:29 +0800

    testing out horizon theme

--- nvim/.config/nvim/lua/dallas/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/dallas/plugins/colorscheme.lua
@@ -1,3 +1,12 @@
+return {
+	"akinsho/horizon.nvim",
+	version = "*",
+	config = function()
+		vim.cmd("colorscheme horizon")
+		vim.o.background = "dark"
+	end,
+}
+
 -- return {
 -- 	"folke/tokyonight.nvim",
 -- 	lazy = false,
@@ -55,34 +64,34 @@
 -- 	end,
 -- }
 
-return {
-	"ellisonleao/gruvbox.nvim",
-	priority = 1000,
-	opts = {
-		terminal_colors = true, -- add neovim terminal colors
-		undercurl = true,
-		underline = true,
-		bold = true,
-		italic = {
-			strings = true,
-			emphasis = true,
-			comments = true,
-			operators = false,
-			folds = true,
-		},
-		strikethrough = true,
-		invert_selection = false,
-		invert_signs = false,
-		invert_tabline = false,
-		inverse = true, -- invert background for search, diffs, statuslines and errors
-		contrast = "hard", -- can be "hard", "soft" or empty string
-		palette_overrides = {},
-		overrides = {},
-		dim_inactive = false,
-		transparent_mode = true,
-	},
-	config = function(_, opts)
-		require("gruvbox").setup(opts)
-		vim.cmd("colorscheme gruvbox")
-	end,
-}
+-- return {
+-- 	"ellisonleao/gruvbox.nvim",
+-- 	priority = 1000,
+-- 	opts = {
+-- 		terminal_colors = true, -- add neovim terminal colors
+-- 		undercurl = true,
+-- 		underline = true,
+-- 		bold = true,
+-- 		italic = {
+-- 			strings = true,
+-- 			emphasis = true,
+-- 			comments = true,
+-- 			operators = false,
+-- 			folds = true,
+-- 		},
+-- 		strikethrough = true,
+-- 		invert_selection = false,
+-- 		invert_signs = false,
+-- 		invert_tabline = false,
+-- 		inverse = true, -- invert background for search, diffs, statuslines and errors
+-- 		contrast = "hard", -- can be "hard", "soft" or empty string
+-- 		palette_overrides = {},
+-- 		overrides = {},
+-- 		dim_inactive = false,
+-- 		transparent_mode = true,
+-- 	},
+-- 	config = function(_, opts)
+-- 		require("gruvbox").setup(opts)
+-- 		vim.cmd("colorscheme gruvbox")
+-- 	end,
+-- }
