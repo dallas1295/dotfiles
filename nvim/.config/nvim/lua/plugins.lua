942562497e3afbc14dfd177a58828b025315d591
Dallas S. <andrewdallassherman@gmail.com>
2026-04-22 13:31:23 +0800

    big updates... messing around.

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -3,15 +3,43 @@ local add = function(s)
 end
 
 -- Theme
+
+add({
+	{ src = "https://github.com/ellisonleao/gruvbox.nvim", name = "gruvbox" },
+})
+require("gruvbox").setup({
+	terminal_colors = true,
+	undercurl = true,
+	underline = true,
+	bold = true,
+	italic = {
+		strings = true,
+		emphasis = true,
+		comments = true,
+		operators = false,
+		folds = true,
+	},
+	strikethrough = true,
+	invert_selection = false,
+	invert_signs = false,
+	invert_tabline = false,
+	inverse = true, -- invert background for search, diffs, statuslines and errors
+	contrast = "", -- can be "hard", "soft" or empty string
+	palette_overrides = {},
+	overrides = {},
+	dim_inactive = false,
+	transparent_mode = true,
+})
+vim.cmd("colorscheme gruvbox")
+
 -- add({
 -- 	{ src = "https://github.com/vague2k/vague.nvim", name = "vague" },
 -- })
 -- vim.cmd("colorscheme vague")
-
-add({
-	{ src = "https://github.com/miikanissi/modus-themes.nvim", name = "modus" },
-})
-vim.cmd("colorscheme modus")
+-- add({
+-- 	{ src = "https://github.com/miikanissi/modus-themes.nvim", name = "modus" },
+-- })
+-- vim.cmd("colorscheme modus")
 
 -- Mini (QoL)
 add({
