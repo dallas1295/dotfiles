711ed4016a48035803e43fd6635c142fa639c7ea
Dallas S. <adsherman@tutamail.com>
2026-06-15 14:06:58 +0800

    fixing

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -38,8 +38,6 @@ alias lla 'lsd -la'
 alias lt 'lsd --tree --depth 2'
 alias lg 'lsd -la --git'
 
-alias mv 'mullvad-tui'
-
 set -gx FZF_DEFAULT_COMMAND "fd --hidden --exclude .git --type f"
 set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
 
