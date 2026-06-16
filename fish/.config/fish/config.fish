15339877503dc846083a61e0993ecf60e8f8a5c8
Dallas S. <adsherman@tutamail.com>
2026-06-16 10:57:54 +0800

    updates

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,4 @@
+set -g fish_greeting
 set -g fish_key_bindings fish_vi_key_bindings
 set -gx SUDOEDITOR nvim
 set -gx EDITOR nvim 
@@ -13,23 +14,20 @@ fish_add_path (go env GOPATH)/bin
 fish_add_path "$HOME/.cargo/bin"
 fish_add_path "$PYENV_ROOT/bin"
 fish_add_path "$HOME/.opencode/bin"
+fish_add_path "$HOME/opt/bin"
 
-for dir in (find "$HOME/opt" -mindepth 1 -maxdepth 3 -type d)
-    fish_add_path "$dir"
-end
-
-alias notes 'cd ~/vaults/stuff/ && nvim'
 alias hx helix
 alias svim 'sudo -E nvim'
 
-alias pacman 'paru'
+# alias pacman 'paru'
 
-# alias poweroff 'loginctl poweroff'
-# alias reboot 'loginctl reboot'
-# alias xi 'sudo xbps-install'
-# alias xr 'sudo xbps-remove'
-# alias xq 'sudo xbps-query'
-# alias xu 'sudo xbps-install -Su'
+alias poweroff 'loginctl poweroff'
+alias reboot 'loginctl reboot'
+alias xi 'sudo xbps-install'
+alias xr 'sudo xbps-remove'
+alias xq 'sudo xbps-query'
+alias xu 'sudo xbps-install -Su'
+alisa vsv 'sudo vsv'
 
 alias ls lsd
 alias ll 'lsd -l'
@@ -42,7 +40,7 @@ set -gx FZF_DEFAULT_COMMAND "fd --hidden --exclude .git --type f"
 set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
 
 zoxide init fish | source
-# starship init fish | source
+starship init fish | source
 
 clear
 # fastfetch
